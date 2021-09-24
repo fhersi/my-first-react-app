6525081b0f9827cd22780c149131c65d63b011d4
F Hersi <33755306+fhersi@users.noreply.github.com>
2021-09-24 17:37:36 +0100

    update - change format date is rendered.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,16 @@
 import "./ExpenseItem.css";
 function ExpenseItem(props) {
+	const day = props.date.toLocaleString("en-GB", { day: "2-digit" });
+	const month = props.date.toLocaleString("en-GB", { month: "long" });
+	const year = props.date.getFullYear();
+
 	return (
 		<div className="expense-item">
-			<div>{props.date.toGMTString()}</div>
+			<div>
+				<div>{month}</div>
+				<div>{day}</div>
+				<div>{year}</div>
+			</div>
 			<div className="expense-item__description">
 				<h2>{props.title}</h2>
 				<div className="expense-item__price">{props.amount}</div>
